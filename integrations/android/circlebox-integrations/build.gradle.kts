23f43f84f5041ba940e8ed1398c584982e792dfe
Jeremiah Oluwaseun Erinola <seunjeremiah@gmail.com>
2026-02-26 15:41:08 +0100

    fix(release): align android integrations version to 0.3.1

--- integrations/android/circlebox-integrations/build.gradle.kts
+++ integrations/android/circlebox-integrations/build.gradle.kts
@@ -1,14 +1,37 @@
 plugins {
     kotlin("jvm") version "1.9.24"
+    id("maven-publish")
 }
 
 group = "com.circlebox.integrations"
-version = "0.3.0"
-
-repositories {
-    mavenCentral()
-}
+version = "0.3.1"
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "com.circlebox.integrations"
+            artifactId = "circlebox-integrations"
+            version = project.version.toString()
+
+            pom {
+                name.set("circlebox-integrations")
+                description.set("Companion mappers for forwarding CircleBox payloads into Sentry and PostHog")
+                url.set("https://github.com/jeremiahseun/circlebox")
+            }
+        }
+    }
 }
